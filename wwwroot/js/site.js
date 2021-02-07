aaa1689a65c2fc04cef0984dd9febb5bbdb54f2f
谭武成 <tanwucheng@MBP.local>
2021-02-08 00:53:05 +0800

    Commit 2021-02-08 00:53:00

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -46,4 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
         false
     );
+
+    const elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, {});
 });
